d5e6857e43dca580be6f22b1b6b7867ac9c0a478
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2025-11-28 14:26:56 -0300

    Alterei o codigo usando o do while, dessa forma, o usuario decide se vai usar a funcionalidade de deposito ou de saque, e não é obrigado a passar por 2 funcionalidades do programa.

--- programaBancarioJava/src/Main.java
+++ programaBancarioJava/src/Main.java
@@ -1,7 +1,6 @@
 import entities.DataHoraStatus;
 import entities.OrderStatus;
 import entities.Usuarios;
-
 import java.util.Date;
 import java.util.Scanner;
 
@@ -9,47 +8,68 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Usuarios usuario1;
-        DataHoraStatus deposited = new DataHoraStatus(new Date(), OrderStatus.DEPOSITED);
-        DataHoraStatus withdrawn = new DataHoraStatus(new Date(), OrderStatus.WITHDRAWN);
+        char opcao;
 
-        System.out.println("Digite os quatro números finais da sua conta:");
+        System.out.println("--- BEM-VINDO AO CAIXA ELETRÔNICO ---");
+        System.out.print("Digite os quatro números finais da sua conta: ");
         int conta = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Digite o seu nome e sobrenome:");
+        System.out.print("Digite o seu nome e sobrenome: ");
         String nome = sc.nextLine();
 
-        System.out.println("Esse é o seu primeiro deposito? (s/n)");
+        System.out.print("É o seu primeiro depósito? (s/n): ");
         char resposta = sc.next().charAt(0);
+
         if (resposta == 's') {
-            System.out.println("Digite o valor do seu deposito inicial:");
+            System.out.print("Digite o valor do depósito inicial: ");
             double valorInicial = sc.nextDouble();
             usuario1 = new Usuarios(conta, nome, valorInicial);
+        } else {
+            usuario1 = new Usuarios(conta, nome, 500);
         }
-        else {
-            usuario1 = new Usuarios(conta, nome);
-        }
-        System.out.println(" ");
-
         sc.nextLine();
 
-        System.out.println(deposited.toString());
-        System.out.println("\nDados Iniciais: " + usuario1.toString());
+        System.out.println("\n*** ACESSO CONCEDIDO ***");
+        System.out.println("Status atual: " + usuario1.toString());
 
-        System.out.println("\nDigite o novo valor do deposito: ");
-        usuario1.depositar(sc.nextDouble());
-        System.out.println(" ");
+        do {
+            System.out.println("\n-------------------------------------");
+            System.out.println("Escolha uma operação:");
+            System.out.println("[D] Depósito");
+            System.out.println("[S] Saque (taxa de R$ 5,00)");
+            System.out.println("[E] Encerrar Sessão");
+            System.out.print("Opção: ");
 
-        System.out.println(deposited.toString());
-        System.out.println(usuario1.toString());
+            opcao = sc.next().toUpperCase().charAt(0);
+            sc.nextLine();
 
-        System.out.println("\nDigite o valor que você deseja sacar: ");
-        usuario1.sacarValor(sc.nextDouble());
-        System.out.println(" ");
+            if (opcao == 'D') {
+                System.out.print("Valor do depósito: ");
+                usuario1.depositar(sc.nextDouble());
 
-        System.out.println(withdrawn.toString());
-        System.out.println(usuario1.toString());
+                DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.DEPOSITED);
+                System.out.println("REGISTRO: " + registro.toString());
+            }
+            else if (opcao == 'S') {
+                System.out.print("Valor do saque: ");
+                usuario1.sacarValor(sc.nextDouble());
 
+                DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.WITHDRAWN);
+                System.out.println("REGISTRO: " + registro.toString());
+            }
+            else if (opcao == 'E') {
+                System.out.println("\nSessão encerrada. Até logo, " + usuario1.getNome() + "!");
+            }
+            else {
+                System.out.println("Opção inválida. Tente D, S ou E.");
+            }
+
+            if (opcao != 'E') {
+                System.out.println("Saldo Atual: " + usuario1.toString());
+            }
+
+        } while (opcao != 'E');
 
         sc.close();
     }
