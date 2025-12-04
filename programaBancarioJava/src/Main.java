fcd6331797cb8db80af07f18e577e2702c736f74
Alex Kauã Silva Garcia <alexkaua502@gmail.com>
2025-12-04 16:20:28 -0300

    Refatora lógica inicial para identificar conta física ou jurídica

--- programaBancarioJava/src/Main.java
+++ programaBancarioJava/src/Main.java
@@ -11,65 +11,77 @@ public class Main {
         char opcao;
 
         System.out.println("--- BEM-VINDO AO CAIXA ELETRÔNICO ---");
-        System.out.print("Digite os quatro números finais da sua conta: ");
-        int conta = sc.nextInt();
-        sc.nextLine();
+        System.out.println();
+        System.out.println("Qual o tipo de conta? [F] Física / [J] Jurídica: ");
+        char tipoConta = sc.next().toUpperCase().charAt(0);
 
-        System.out.print("Digite o seu nome e sobrenome: ");
-        String nome = sc.nextLine();
+        if (tipoConta == 'F') {
 
-        System.out.print("É o seu primeiro depósito? (s/n): ");
-        char resposta = sc.next().charAt(0);
-
-        if (resposta == 's') {
-            System.out.print("Digite o valor do depósito inicial: ");
-            double valorInicial = sc.nextDouble();
-            usuario1 = new Usuarios(conta, nome, valorInicial);
-        } else {
-            usuario1 = new Usuarios(conta, nome, 500);
-        }
-        sc.nextLine();
-
-        System.out.println("\n*** ACESSO CONCEDIDO ***");
-        System.out.println("Status atual: " + usuario1.toString());
-
-        do {
-            System.out.println("\n-------------------------------------");
-            System.out.println("Escolha uma operação:");
-            System.out.println("[D] Depósito");
-            System.out.println("[S] Saque (taxa de R$ 5,00)");
-            System.out.println("[E] Encerrar Sessão");
-            System.out.print("Opção: ");
-
-            opcao = sc.next().toUpperCase().charAt(0);
+            System.out.print("Digite os quatro números finais da sua conta: ");
+            int conta = sc.nextInt();
             sc.nextLine();
 
-            if (opcao == 'D') {
-                System.out.print("Valor do depósito: ");
-                usuario1.depositar(sc.nextDouble());
+            System.out.print("Digite o seu nome e sobrenome: ");
+            String nome = sc.nextLine();
 
-                DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.DEPOSITED);
-                System.out.println("REGISTRO: " + registro.toString());
-            }
-            else if (opcao == 'S') {
-                System.out.print("Valor do saque: ");
-                usuario1.sacarValor(sc.nextDouble());
+            System.out.print("É o seu primeiro depósito? (s/n): ");
+            char resposta = sc.next().charAt(0);
 
-                DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.WITHDRAWN);
-                System.out.println("REGISTRO: " + registro.toString());
+            if (resposta == 's') {
+                System.out.print("Digite o valor do depósito inicial: ");
+                double valorInicial = sc.nextDouble();
+                usuario1 = new Usuarios(conta, nome, valorInicial);
+            } else {
+                usuario1 = new Usuarios(conta, nome, 500);
             }
-            else if (opcao == 'E') {
-                System.out.println("\nSessão encerrada. Até logo, " + usuario1.getNome() + "!");
-            }
-            else {
-                System.out.println("Opção inválida. Tente D, S ou E.");
-            }
+            sc.nextLine();
 
-            if (opcao != 'E') {
-                System.out.println("Saldo Atual: " + usuario1.toString());
-            }
+            System.out.println("\n*** ACESSO CONCEDIDO ***");
+            System.out.println("Status atual: " + usuario1.toString());
 
-        } while (opcao != 'E');
+            do {
+                System.out.println("\n-------------------------------------");
+                System.out.println("Escolha uma operação:");
+                System.out.println("[D] Depósito");
+                System.out.println("[S] Saque (taxa de R$ 5,00)");
+                System.out.println("[E] Encerrar Sessão");
+                System.out.print("Opção: ");
+
+                opcao = sc.next().toUpperCase().charAt(0);
+                sc.nextLine();
+
+                if (opcao == 'D') {
+                    System.out.print("Valor do depósito: ");
+                    usuario1.depositar(sc.nextDouble());
+                    System.out.println();
+
+                    DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.DEPOSITED);
+                    System.out.println(registro.toString());
+                }
+                else if (opcao == 'S') {
+                    System.out.print("Valor do saque: ");
+                    usuario1.sacarValor(sc.nextDouble());
+                    System.out.println();
+
+                    DataHoraStatus registro = new DataHoraStatus(new Date(), OrderStatus.WITHDRAWN);
+                    System.out.println(registro.toString());
+                }
+                else if (opcao == 'E') {
+                    System.out.println("\nSessão encerrada. Até logo, " + usuario1.getNome() + "!");
+                }
+                else {
+                    System.out.println("Opção inválida. Tente D, S ou E.");
+                }
+
+                if (opcao != 'E') {
+                    System.out.println("Saldo Atual: " + usuario1.toString());
+                }
+
+            } while (opcao != 'E');
+
+        } else if (tipoConta == 'J') {
+            System.out.println("teste");
+        }
 
         sc.close();
     }
